7d6723deb37dec5c7264701f489e5b9810f07987
Bruno Garcia Moura <bgarciamoura@gmail.com>
2022-08-20 19:45:06 -0300

    refact(duplicates): Created the function for sum if there is two or more transactions in the same day

--- main.py
+++ main.py
@@ -7,16 +7,7 @@ from src.investments import investments
 import numpy_financial as npf
 
 
-cashflows = []
 
-def sortByCreatedAt(e):
-    return e['created_at']
-
-def sortByDue(e):
-    return e['due_date']
-
-def sortByDate(e):
-    return e['date']
 
 def getDatesRange(startDate, endDate):
     dates = []
@@ -25,88 +16,84 @@ def getDatesRange(startDate, endDate):
         startDate += timedelta(days=1)
     return dates
 
+def sort_by_date(e):
+    return e['date']
 
+def sum_duplicates(original_list, destination_list, key):
+    index = 0
+    date_key = 'created_at' if key == 'investments' else 'due_date'
+    for item in original_list:
+        previousItem = original_list[index - 1] if index - 1 < len(original_list) else None
+        nextItem = original_list[index + 1] if index + 1 < len(original_list) else None
+        if previousItem and item[date_key] == previousItem[date_key]:
+            continue
+        elif nextItem and item[date_key] == nextItem[date_key]:
+            if key == 'installments':
+              newItem = {
+                  'investment_id': item['investment_id'],
+                  'due_date': item['due_date'],
+                  'amount': str(float(item['amount']) + float(nextItem['amount']))
+              }
+            else:
+              newItem = {
+                  'id': item['id'],
+                  'created_at': item['created_at'],
+                  'amount': str(float(item['amount']) + float(nextItem['amount']))
+              }
+            destination_list.append(newItem)
+            index += 1
+        else:
+            destination_list.append(item)
+        index += 1
 
 def calc_irr():
     data = []
     computedInstallments = []
     computedInvestments = []
-
-    investments.investments.sort(key=sortByCreatedAt('created_at'))
-    installments.installments.sort(key=sortByDue)
-
+    cashflows = []
     index = 0 
-    for installment in installments.installments:
-        previousInstallment = installments.installments[index - 1] if index - 1 < len(installments.installments) else None
-        nextInstallment = installments.installments[index + 1] if index + 1 < len(installments.installments) else None
-        if previousInstallment and installment['due_date'] == previousInstallment['due_date']:
-            continue
-        elif nextInstallment and installment['due_date'] == nextInstallment['due_date']:
-            newInstallment = {
-                'investment_id': installment['investment_id'],
-                'due_date': installment['due_date'],
-                'amount': str(float(installment['amount']) + float(nextInstallment['amount']))
-            }
-            computedInstallments.append(newInstallment)
-            index += 1
-        else:
-            computedInstallments.append(installment)
-        index += 1
+
+    investmentsTransactions = sorted(investments.investments, key=lambda k: k['created_at'], reverse=False)
+    installmentsTransactions = sorted(installments.installments, key=lambda k: k['due_date'], reverse=False)
+
+    sum_duplicates(investmentsTransactions, computedInvestments, 'investments')
+    sum_duplicates(installmentsTransactions, computedInstallments, 'installments')
 
 
-    index = 0
-    for investment in investments.investments:
-        previousInvestment = investments.investments[index - 1] if index - 1 < len(investments.investments) else None
-        nextInvestment = investments.investments[index + 1] if index + 1 < len(investments.investments) else None
-        if previousInvestment and investment['created_at'] == previousInvestment['created_at']:
-            continue
-        elif nextInvestment and investment['created_at'] == nextInvestment['created_at']:
-            newInvestment = {
-                'id': investment['id'],
-                'created_at': investment['created_at'],
-                'amount': str(float(investment['amount']) + float(nextInvestment['amount']))
-            }
-            computedInvestments.append(newInvestment)
-            index += 1
-        else:
-            computedInvestments.append(investment)
-        index += 1
+    # for investment in computedInvestments:
+    #   cashflows.append({
+    #     'date': investment['created_at'],
+    #     'amount': float(investment['amount']) * -1
+    #   })
 
+    # for installment in computedInstallments:
+    #   cashflows.append({
+    #     'date': installment['due_date'],
+    #     'amount': float(installment['amount'])
+    #   })
 
-    for investment in computedInvestments:
-      cashflows.append({
-        'date': investment['created_at'],
-        'amount': float(investment['amount']) * -1
-      })
-
-    for installment in computedInstallments:
-      cashflows.append({
-        'date': installment['due_date'],
-        'amount': float(installment['amount'])
-      })
-
-    cashflows.sort(key=sortByDate)
-    startDate = date.fromisoformat(cashflows[0]['date'])
-    endDate = date.fromisoformat(cashflows[-1]['date'])
-    dates = getDatesRange(startDate, endDate)
+    # cashflows.sort(key=sort_by_date)
+    # startDate = date.fromisoformat(cashflows[0]['date'])
+    # endDate = date.fromisoformat(cashflows[-1]['date'])
+    # dates = getDatesRange(startDate, endDate)
     
-    for singleDate in dates:
-      teste = list(filter(lambda x: x['date'] == singleDate.isoformat(), cashflows))
-      if len(teste) < 1:
-        cashflows.append({
-          'date': singleDate.isoformat(),
-          'amount': 0.00
-        })
+    # for singleDate in dates:
+    #   teste = list(filter(lambda x: x['date'] == singleDate.isoformat(), cashflows))
+    #   if len(teste) < 1:
+    #     cashflows.append({
+    #       'date': singleDate.isoformat(),
+    #       'amount': 0.00
+    #     })
     
-    cashflows.sort(key=sortByDate)
+    # cashflows.sort(key=sort_by_date)
 
-    some = 0
-    for transaction in cashflows:
-      data.append(transaction['amount'])
-      some += transaction['amount']
+    # some = 0
+    # for transaction in cashflows:
+    #   data.append(transaction['amount'])
+    #   some += transaction['amount']
     
-    irr = round(npf.irr(data), 2)
-    print("TIR:", irr)
+    # irr = round(npf.irr(data), 2)
+    # print("TIR:", irr)
 
 
 if __name__ == "__main__":
