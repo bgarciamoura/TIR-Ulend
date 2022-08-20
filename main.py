29bcc12b63766a2bfe3e553ee47c96018282a53d
Bruno Garcia Moura <bgarciamoura@gmail.com>
2022-08-20 20:06:47 -0300

    refact(duplicates): Created the functions for manipulate the cash flow

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ from src.investments import investments
 import numpy_financial as npf
 
 
-
-
 def getDatesRange(startDate, endDate):
     dates = []
     while startDate <= endDate:
@@ -46,12 +44,42 @@ def sum_duplicates(original_list, destination_list, key):
             destination_list.append(item)
         index += 1
 
+def mount_cash_flow(investments, installments, cash_flows):
+  for investment in investments:
+    cash_flows.append({
+      'date': investment['created_at'],
+      'amount': float(investment['amount']) * -1
+    })
+
+  for installment in installments:
+    cash_flows.append({
+      'date': installment['due_date'],
+      'amount': float(installment['amount'])
+    })
+
+  cash_flows.sort(key=sort_by_date)
+
+  include_dates_in_cash_flows(cash_flows)
+  
+  cash_flows.sort(key=sort_by_date)
+
+def include_dates_in_cash_flows(cash_flows):
+    startDate = date.fromisoformat(cash_flows[0]['date'])
+    endDate = date.fromisoformat(cash_flows[-1]['date'])
+    dates = getDatesRange(startDate, endDate)
+    for singleDate in dates:
+      teste = list(filter(lambda x: x['date'] == singleDate.isoformat(), cash_flows))
+      if len(teste) < 1:
+        cash_flows.append({
+          'date': singleDate.isoformat(),
+          'amount': 0.00
+        })
+
 def calc_irr():
     data = []
     computedInstallments = []
     computedInvestments = []
     cashflows = []
-    index = 0 
 
     investmentsTransactions = sorted(investments.investments, key=lambda k: k['created_at'], reverse=False)
     installmentsTransactions = sorted(installments.installments, key=lambda k: k['due_date'], reverse=False)
@@ -59,41 +87,13 @@ def calc_irr():
     sum_duplicates(investmentsTransactions, computedInvestments, 'investments')
     sum_duplicates(installmentsTransactions, computedInstallments, 'installments')
 
+    mount_cash_flow(computedInvestments, computedInstallments, cashflows)
 
-    # for investment in computedInvestments:
-    #   cashflows.append({
-    #     'date': investment['created_at'],
-    #     'amount': float(investment['amount']) * -1
-    #   })
-
-    # for installment in computedInstallments:
-    #   cashflows.append({
-    #     'date': installment['due_date'],
-    #     'amount': float(installment['amount'])
-    #   })
-
-    # cashflows.sort(key=sort_by_date)
-    # startDate = date.fromisoformat(cashflows[0]['date'])
-    # endDate = date.fromisoformat(cashflows[-1]['date'])
-    # dates = getDatesRange(startDate, endDate)
+    for transaction in cashflows:
+      data.append(transaction['amount'])
     
-    # for singleDate in dates:
-    #   teste = list(filter(lambda x: x['date'] == singleDate.isoformat(), cashflows))
-    #   if len(teste) < 1:
-    #     cashflows.append({
-    #       'date': singleDate.isoformat(),
-    #       'amount': 0.00
-    #     })
-    
-    # cashflows.sort(key=sort_by_date)
-
-    # some = 0
-    # for transaction in cashflows:
-    #   data.append(transaction['amount'])
-    #   some += transaction['amount']
-    
-    # irr = round(npf.irr(data), 2)
-    # print("TIR:", irr)
+    irr = round(npf.irr(data), 2)
+    print("TIR:", irr)
 
 
 if __name__ == "__main__":
